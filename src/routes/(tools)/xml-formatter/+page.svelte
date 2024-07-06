0f8b25b25e38cee296393baaafe7a10de2415358
Akash Gaikwad <gaikwadakash722@gmail.com>
2024-07-06 15:36:37 +0530

    added load button

--- src/routes/(tools)/xml-formatter/+page.svelte
+++ src/routes/(tools)/xml-formatter/+page.svelte
@@ -112,6 +112,10 @@
         >
           <button
             class="px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+            on:click={formatXML}>Load</button
+          >
+          <button
+            class="px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
             on:click={formatXML}>Format</button
           >
         </div>
